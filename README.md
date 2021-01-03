a57049556ce3a9d6c8a5b6e7314e724f485b18b1
Jon Laboy <jonlaboyoc@gmail.com>
2021-01-03 11:18:01 -0500

    Updates

--- README.md
+++
@@ -1,6 +0,0 @@
-
-## React Restaurant App 
-
-In this project the user can view restaurants based on their geo-location. The user can view information about the restaurants and add their own reviews. The user can also add newly discovered restaurants to the map with their own information. 
-
-
---
+++ precache-manifest.472f44bb12890027f3e9111d4662e503.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b141830a26bfc31a501835d167296783",
+    "url": "/react-restaurant-app/index.html"
+  },
+  {
+    "revision": "549ce2012c802f1f671f",
+    "url": "/react-restaurant-app/static/css/main.9ab7901b.chunk.css"
+  },
+  {
+    "revision": "292e792593a59bcf093b",
+    "url": "/react-restaurant-app/static/js/2.bfbd173d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-restaurant-app/static/js/2.bfbd173d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "549ce2012c802f1f671f",
+    "url": "/react-restaurant-app/static/js/main.108c146a.chunk.js"
+  },
+  {
+    "revision": "54bc1c53c1fb74310559",
+    "url": "/react-restaurant-app/static/js/runtime-main.26ef617f.js"
+  },
+  {
+    "revision": "9a6884b3d8c02aff1cc6ffca37f40ecb",
+    "url": "/react-restaurant-app/static/media/person-icon.9a6884b3.png"
+  },
+  {
+    "revision": "e43fad9fe23e27afb86a88f9df516ddf",
+    "url": "/react-restaurant-app/static/media/restaurant-icon.e43fad9f.png"
+  }
+]);
